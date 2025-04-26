e8d67fe32521743a7daeeea1bf21d130703237c1
Justin Venable <dartsolarpunk@gmail.com>
2025-04-26 13:50:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ class User(db.Model):
 
 
 
-@app.route('/')
-def index():
-    return 'Autumn AI Chat Home'
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
