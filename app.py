cc61e2d31013d04224420faa55a554486c4f91d2
Justin Venable <dartsolarpunk@gmail.com>
2025-04-26 13:39:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 @app.route('/')
 def index():
